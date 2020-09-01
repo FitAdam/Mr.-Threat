936dd805041a351c6291fefecc173db3df20e2d2
FitAdam <adam.george617@gmail.com>
2020-09-01 22:46:27 +0100

    virus total dict

--- web_app/virus_total.py
+++ web_app/virus_total.py
@@ -1,7 +1,7 @@
 import requests
 import json
-# key
 
+# key
 from keys import virus_total_key
 
 
@@ -20,10 +20,24 @@ def get_headers(virus_total_key):
     }
     return headers
 
+def get_output(new_response):
+    """This function using json.loads turns data into a python dictionary."""
+    decodedResponse = json.loads(new_response.text)
+    # Formatted output
+    return decodedResponse
 
-headers = get_headers(virus_total_key)
-'querystring = {"limit":"1"}
-url = get_url_with_ip('23.90.145.44')
-response = requests.request("GET", url, headers=headers)
+def check_the_ip(the_ip):
+    """This function takes user input, checks the IP and returns the answer"""
+    headers = get_headers(virus_total_key)
+    url = get_url_with_ip(the_ip)
+    response = requests.request("GET", url, headers=headers)
+    answer = get_output(response)
+    return answer
 
-print(response.text)
+
+checked_dict = check_the_ip('185.176.27.46')
+checked_dict = checked_dict['data']['attributes']
+
+for value, key in checked_dict.items():
+    print(key)
+    
